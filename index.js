2ccc814bd5db2cf9367eefd13d441126996d12d7
Malinali <mali@devf.mx>
2019-09-24 20:33:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { GraphQLServer } = require('graphql-yoga');
+// Acento que respeta espacios y salto de linea.
+// tipo Query
+// nombre(parametros): resultadoEnTipoDeDato 
+const typeDefs = `
+
+    type Query{
+        hello(name:String): String!
+    }
+
+`
+
+const resolvers = {
+    Query:{
+        hello:(root, params, context, info) => `Hola ${params.name}` //interpolación back tips //acento grave
+    }
+}
