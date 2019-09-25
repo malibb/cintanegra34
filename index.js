ff46cde014190d240f2a42970188c3a5262f57a2
Malinali <mali@devf.mx>
2019-09-24 21:10:03 -0500

    first example graphql query and mutations

--- index.js
+++ index.js
@@ -7,13 +7,36 @@ const typeDefs = `
 
     type Query{
         hello(name:String): String!
+        getUsers: [User!]
+    }
+
+    type Mutation{
+        createUser(name:String!, age:Int!):User
+    }
+
+    type User{
+        id:Int!
+        name:String!
+        age:Int!
     }
 
 `
-
+const users = [];
 const resolvers = {
     Query:{
-        hello:(root, params, context, info) => `Hola ${params.name}` //interpolación back tips //acento grave
+        hello:(root, params, context, info) => `Hola ${params.name}`, //interpolación back tips //acento grave
+        getUsers:(root, params, context, info) => users
+    },
+    Mutation:{
+        createUser:(root, params, context, info) => {
+            const user = {
+                id: users.length + 1,
+                name: params.name,
+                age: params.age
+            };
+            users.push(user);
+            return user;
+        }
     }
 }
 
